0c1eb07ae1ff233b8da3c53faa2a4b1d93514807
Louka-Tech <157493143+Louka-Tech@users.noreply.github.com>
2024-04-04 11:55:25 +0200

    Update Décompte.py

--- Décompte.py
+++ Décompte.py
@@ -8,6 +8,12 @@ def ImportModules():#importation des modules
                 import time, datetime
                 print("super!")
                 date_actuelle = datetime.date.today()
+                print(date_actuelle)
+                day = []
+                month = []
+                Year = []
         except ModuleNotFound :
                 print("modulenotfound")
 ImportModules()
+def UserEntry():
+        EntereDate = str(input("Enter the date on YYYY/MM/DD"))
