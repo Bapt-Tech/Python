d97bbef375b092c46aaa0789b608d8f66f93a35b
Louka-Tech <157493143+Louka-Tech@users.noreply.github.com>
2024-04-04 11:41:09 +0200

    Update Décompte.py

--- Décompte.py
+++ Décompte.py
@@ -3,4 +3,10 @@
 ###04/04/2024
 ###11:28
 
-def(ImportModules):
+def ImportModules():#importation des modules
+        try:
+                import time, datetime
+                print("super!")
+        except ModuleNotFound :
+                print("modulenotfound")
+ImportModules()
