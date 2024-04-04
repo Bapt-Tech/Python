27bedf77af527ff6dee41c4c882055f9c5f6a755
Bapt-Tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-04-04 11:36:47 +0200

    Create DécompteB.py

---
+++ DécompteB.py
@@ -0,0 +1,12 @@
+###Bapt-Tech
+###Louka-Tech
+###04/04/2024
+###11:28
+
+def ImportModules(): #Importataion des modules.
+        try:
+                import time, datetime
+        except ModuleNotFound:
+                print("Modules non trouvés.")
+                
+
